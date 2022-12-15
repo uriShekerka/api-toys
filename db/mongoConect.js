dd9ace1f636bd10fe548dc9b2acec07bdfc920ab
uriShekerka <116493322+uriShekerka@users.noreply.github.com>
2022-12-15 09:22:22 +0200

    Add files via upload

--- db/mongoConect.js
+++ db/mongoConect.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const { config } = require("../config/secret");
 
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://uri:1234@cluster0.v0gts8j.mongodb.net/idf77');
+  await mongoose.connect(`mongodb+srv://${config.userDB}:${config.userPass}@cluster0.v0gts8j.mongodb.net/idf77`);
    console.log("toys work")
   // use `await mongoose.connect('mongodb://user:passwormongodb+srv://<username>:<password>@cluster0.v0gts8j.mongodb.net/testd@localhost:27017/test');` if your database has auth enabled
 }
